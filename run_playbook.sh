48fb20365b3ce62ea120764ea55abac3c9516e16
vijay.singh-edifecs <vijay.singh@edifecs.com>
2018-06-19 16:25:15 +0530

    Removed bash script file

--- run_playbook.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo ansible-playbook ec2_playbook.yml --extra-vars instancetype=t2.micro securitygroup=ansible-webserver awsaminame=ami-41e9c52e keypairname=sample regionname=ap-south-1 dbpasswd=password
